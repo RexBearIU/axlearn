e98facbb3cd3d61669c1e205adf5bcb4318ea422
Sujeeth Jinesh <sujinesh@google.com>
2025-07-11 20:35:26 +0000

    Update params to work for number of devices

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -860,8 +860,9 @@ def get_trainer_kwargs(
             ),
             learner_kwargs=dict(peak_lr=1.5e-4, weight_decay=0.1),
             max_sequence_length=max_sequence_length,
-            train_batch_size=train_batch_size,
-            max_step=max_step,
+            train_batch_size=len(jax.devices()), # train_batch_size,
+            max_step=10_000, # max_step,
+            save_every_n_steps=100,
             mesh_shape=mesh_shape_from_axes(data=-1, fsdp=64, model=4),
             mesh_rules=(
                 (
